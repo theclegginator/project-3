6d7290a5555d5b56e4d3db075ed62818e41dd2eb
Vyjoo Frank <vlakshm26@gmail.com>
2019-07-25 11:16:49 -0400

    added  new font

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,41 +3,51 @@ import { Link } from "react-router-dom";
 import "./style.css";
 
 class Nav extends Component {
-  state = {
-    open: false,
-    width: window.innerWidth
-  };
+  // state = {
+  //   open: false,
+  //   width: window.innerWidth
+  // };
 
-  updateWidth = () => {
-    const newState = { width: window.innerWidth };
-
-    if (this.state.open && newState.width > 991) {
-      newState.open = false;
-    }
-
-    this.setState(newState);
-  };
-
-  toggleNav = () => {
-    this.setState({ open: !this.state.open });
-  };
-
-  componentDidMount() {
-    window.addEventListener("resize", this.updateWidth);
+  navClick () {
+    const wrapper = document.getElementById('wrapper');
+    wrapper.classList.toggle('is-nav-open')
   }
 
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateWidth);
-  }
+  // updateWidth = () => {
+  //   const newState = { width: window.innerWidth };
+
+  //   if (this.state.open && newState.width > 991) {
+  //     newState.open = false;
+  //   }
+
+  //   this.setState(newState);
+  // };
+
+  // toggleNav = () => {
+  //   this.setState({ open: !this.state.open });
+  // };
+
+  // componentDidMount() {
+  //   window.addEventListener("resize", this.updateWidth);
+  // }
+
+  // componentWillUnmount() {
+  //   window.removeEventListener("resize", this.updateWidth);
+  // }
 
   render() {
     return (
-      <nav className="navbar" role="navigation" aria-label="dropdown navigation">
-        <Link className="navbar-brand" to="/">
-        <h1>DOPE COFFEE NAME HERE!</h1>
-        </Link>
-        <div className="navbar-item has-dropdown is-active">
-          <ul className="navbar-nav">
+      <div id="wrapper" className="wrapper">
+      <div className="nav">
+        <div className="navicon" type="menu-fold" onClick={() => this.navClick()} />
+        <div className="navlink">
+
+        <Link className="header-nav__item" to="/">Drinks</Link>
+            <Link className="header-nav__item" to="/Shops/">Search</Link>
+            <Link className="header-nav__item" to="/Home/">Login</Link>
+            {/* <Link className="header-nav__item" to="/Recipe/">Recipe</Link> */}
+        
+{/*  
             <li className="navbar-link">
               <Link
                 onClick={this.toggleNav}
@@ -52,7 +62,7 @@ class Nav extends Component {
               <li className="navbar-item">
                  <Link
                   onClick={this.toggleNav}
-                  className={window.location.pathname === "/shops" ? "nav-link active" : "nav-link"}
+                  className={window.location.pathname === "/shops" ? "nav-link inactive" : "nav-link"}
                   to="/shops"
                 >
                   Search
@@ -78,11 +88,26 @@ class Nav extends Component {
                 Drinks
               </Link>
             </li>
-            </ul>
-            </div>
+           */}
+        </div>
+        
+      </div>
+    </div>
+
+
+
+
+
+      // <nav className="navbar" role="navigation" aria-label="dropdown navigation">
+      //   <Link className="navbar-brand" to="/">
+      //   <h1>DOPE COFFEE NAME HERE!</h1>
+      //   </Link>
+      //   <div className="navbar-item has-dropdown is-active">
+
+      //       </div>
 
       
-      </nav>
+      // </nav>
 
         );
       }
