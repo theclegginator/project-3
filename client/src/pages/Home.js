f5d5779ba4bbde3add5d7ee007edb051f0072fe3
Vyjoo Frank <vlakshm26@gmail.com>
2019-07-25 17:43:10 -0400

    styled login page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,9 +7,16 @@ class Home extends Component {
 
 render() {
     return (
+//      <div className="image">
+//          <image src="https://images.unsplash.com/photo-1494346480775-936a9f0d0877?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3806&q=80"> alt"image"</image>
+// <div className="title">
+
+// Coffee App Name
+       <Form>
+                 </Form>
+
      
-             <Form />
-         
+                      
           
         );
     }
