4f2675529238f3c4817edd6afdcc09be0079841f
L-Train <train19@gmail.com>
2019-07-26 08:10:34 -0400

    Tweaks to ShopList Component

--- client/src/components/ShopList/index.js
+++ client/src/components/ShopList/index.js
@@ -3,15 +3,19 @@ import "./style.css";
 
 function ShopList(props) {
   return (
-    <ul className="list-group">
+    <div className="list-group">
+      <hr />
       {props.results.map(result => (
-        <li className="list-group-item" key={result.id}>
+        <div className="list-group-item" key={result.id}>
           <h1>{result.name}</h1>
           <h2>{result.vicinity}</h2>
+          <h3>{result.rating} - {result.user_ratings_total} reviews</h3>
           <h3>{result.opening_hours.open_now}</h3>
-        </li>
+          <hr />
+          
+        </div>
       ))}
-    </ul>
+    </div>
   );
 }
 
