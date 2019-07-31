5513e1e60378c24ca9a9e7110a6d5ace1db69765
Vyjoo Frank <vlakshm26@gmail.com>
2019-07-30 20:58:58 -0400

    auth added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import DrinksList from './pages/DrinksList';
 import Shops from './pages/Shops';
 import Recipe from './pages/Recipe';
-import Home from './pages/Home'
+// import Home from './pages/Home'
 import Nav from "./components/Nav"
-import { Container } from "./components/Grid";
+// import { Container } from "./components/Grid";
 
+import { SecureRoute, ImplicitCallback } from '@okta/okta-react';
+import Navigation from './components/Shared/Navigation';
+import Homepage from './components/Home/Homepage';
+import RegistrationForm from './components/Auth/RegistrationForm';
+import config from './app.config';
+import LoginPage from './components/Auth/LoginPage';
+import ProfilePage from './components/Auth/ProfilePage';
 // import { Link } from "./../../routes";
+
+ 
+
+
+
 
 class App extends Component {
 
@@ -17,22 +29,22 @@ class App extends Component {
       <div className="App">
         <Router>
           <div>
-            <Nav />
+            <Nav/>
+            <Navigation />
             <Switch>
               <Route exact path="/" component={DrinksList} />
               <Route exact path="/shops" component={Shops} />
               <Route exact path="/recipe" component={Recipe} />
-              <Route exact path="/home" component={Home} />
+              <Route exact path="/home" component={Homepage} />
+              <Route path="/login" render={() => <LoginPage baseUrl={config.url} />} />
+              <Route path="/implicit/callback" component={ImplicitCallback} />
+              <Route path="/register" component={RegistrationForm} />
+              <SecureRoute path="/profile" component={ProfilePage} />
               {/* <Route exact path="/recipe/:id" component={Recipe} /> */}
             </Switch>
           </div>
         </Router>
-        <div>
-          <Container>
-
-
-          </Container>
-        </div>
+     
         </div>
         );
       };
