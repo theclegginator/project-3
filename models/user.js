cdb4bc8a336f4ca9f123eeed2a6db0233146386a
L-Train <train19@gmail.com>
2019-08-08 09:04:44 -0400

    Shop Favorites and Clean Up

--- models/user.js
+++ models/user.js
@@ -6,13 +6,17 @@ const userSchema = new Schema({
   clientId: { type: String, required: true, unique: true },
   faveDrinks: { type: Array },
   userDrinks: { type: Object },
-  faveShops: { type: Array, unique: true },
-  banShops: { type: Array, unique: true }
+  faveShops: { type: Array },
+  banShops: { type: Array }
 
 });
 
 const User = mongoose.model("User", userSchema);
 
 userSchema.plugin(uniqueValidator);
+
+// Make Mongoose use `findOneAndUpdate()`. Note that this option is `true`
+// by default, you need to set it to false.
+mongoose.set('useFindAndModify', false);
 
 module.exports = User;
